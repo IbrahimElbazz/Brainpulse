1432f023b9773508bc97c5b06138b6222101693f
Ibrahim Elbaz <ibrahim.elbaz.1011@gmail.com>
2025-01-31 14:39:05 +0200

    brain screen doneeeeeeeee

--- lib/features/brain/presentation/screens/brain_screen.dart
+++ lib/features/brain/presentation/screens/brain_screen.dart
@@ -1,4 +1,7 @@
+import 'package:brain_pulse/core/Widgets/gap.dart';
+import 'package:brain_pulse/features/brain/presentation/widgets/point_brain.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class BrainScreen extends StatelessWidget {
   const BrainScreen({super.key});
@@ -7,19 +10,125 @@ class BrainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('Brain'),
-        centerTitle: true,
-      ),
-      body: Column(
+      // appBar: AppBar(
+      //   title: const Text('Brain'),
+      //   backgroundColor: Colors.white,
+      //   surfaceTintColor: Colors.white,
+      //   centerTitle: true,
+      // ),
+      body: Stack(
         children: [
           Center(
             child: Image.asset(
-              'assets/images/brain.png',
-              width: 500,
-              height: 500,
+              'assets/images/btain.jpg',
+              width: 600.w,
+              height: 600.h,
             ),
-          )
+          ),
+          Positioned(
+            top: 150.h,
+            left: 170.w,
+            child: const PointInBrain(
+              num: '1',
+            ),
+          ),
+          Positioned(
+            top: 220.h,
+            left: 100.w,
+            child: const PointInBrain(
+              num: '2',
+            ),
+          ),
+          Positioned(
+            top: 220.h,
+            left: 240.w,
+            child: const PointInBrain(
+              num: '3',
+            ),
+          ),
+          Positioned(
+            top: 300.h,
+            left: 170.w,
+            child: const PointInBrain(
+              num: '4',
+            ),
+          ),
+          Positioned(
+            top: 370.h,
+            left: 270.w,
+            child: const PointInBrain(
+              num: '5',
+            ),
+          ),
+          Positioned(
+            top: 370.h,
+            left: 80.w,
+            child: const PointInBrain(
+              num: '6',
+            ),
+          ),
+          Positioned(
+            top: 420.h,
+            left: 170.w,
+            child: const PointInBrain(
+              num: '7',
+            ),
+          ),
+          Positioned(
+            top: 510.h,
+            left: 170.w,
+            child: const PointInBrain(
+              num: '8',
+            ),
+          ),
+          Positioned(
+            top: 600.h,
+            left: 40.w,
+            child: Container(
+              width: 300.w,
+              height: 120.h,
+              decoration: BoxDecoration(
+                color: Colors.grey.withOpacity(0.2),
+                borderRadius: BorderRadius.circular(16.r),
+              ),
+              child: Column(
+                children: [
+                  const GapH(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      PointInBrainInfo(
+                        colorIcon: Colors.blue.withOpacity(0.9),
+                      ),
+                      Text(
+                        'active',
+                        style: TextStyle(
+                          fontSize: 22.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                  const GapH(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      PointInBrainInfo(
+                        colorIcon: Colors.red.withOpacity(0.9),
+                      ),
+                      Text(
+                        'disactive',
+                        style: TextStyle(
+                          fontSize: 22.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
