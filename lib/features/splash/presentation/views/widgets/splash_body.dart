f89b78e9e2b95d384070eedbe5d202e58d6e8905
M0alaa330 <mohassan08563@gmail.com>
2025-03-07 17:08:30 +0200

    updates

--- lib/features/splash/presentation/views/widgets/splash_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_body.dart
@@ -2,8 +2,6 @@ import 'package:brain_pulse/core/Theming/colors.dart';
 import 'package:brain_pulse/core/routing/routers.dart';
 import 'package:brain_pulse/features/auth/login/presentation/controller/cubit/login_cubit.dart';
 import 'package:brain_pulse/features/auth/login/presentation/controller/cubit/login_state.dart';
-import 'package:brain_pulse/features/home/presentation/screens/home_screen.dart';
-import 'package:brain_pulse/features/onBoarding/ui/screen/onBoadrding.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
